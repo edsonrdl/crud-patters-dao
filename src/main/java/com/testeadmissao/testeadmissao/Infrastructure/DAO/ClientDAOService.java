a21ac49b3d5de6811767499d4021c9c502c93ccf
edsonrdl <91911044+edsonrdl@users.noreply.github.com>
2024-04-08 00:01:11 -0300

    feat:Corrigido

---
+++ src/main/java/com/testeadmissao/testeadmissao/Infrastructure/DAO/ClientDAOService.java
@@ -0,0 +1,42 @@
+package com.testeadmissao.testeadmissao.infrastructure.dao;
+import java.util.List;
+import org.springframework.stereotype.Service;
+import com.testeadmissao.testeadmissao.domain.interfaces.useCases.IGenericDAO;
+import com.testeadmissao.testeadmissao.infrastructure.model.ClientEntity;
+
+
+@Service
+public class ClientDAOService implements IGenericDAO<ClientEntity, Long> {
+
+    private final IGenericDAO<ClientEntity, Long> clientDAO;
+   
+    public ClientDAOService(IGenericDAO<ClientEntity, Long> clientDAO) {
+        this.clientDAO = clientDAO;
+    }
+
+    @Override
+    public ClientEntity findById(Long id) {
+        return clientDAO.findById(id);
+    }
+
+    @Override
+    public List<ClientEntity> findAll() {
+        return clientDAO.findAll();
+    }
+
+    @Override
+    public ClientEntity save(ClientEntity clientEntity) {
+        return clientDAO.save(clientEntity);
+    }
+
+    @Override
+    public void update(ClientEntity clientEntity) {
+        clientDAO.update(clientEntity);
+    }
+    
+
+    @Override
+    public void delete(Long id) {
+        clientDAO.delete(id);
+    }
+}
