de3ef779cf8833390eb784604ee606c815aa323f
edsonrdl <91911044+edsonrdl@users.noreply.github.com>
2024-04-08 05:33:32 -0300

    feat:Finalizado

---
+++ src/main/java/com/testeadmissao/testeadmissao/controllers/IndexController.java
@@ -0,0 +1,15 @@
+package com.testeadmissao.testeadmissao.controllers;
+
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Order
+@Controller
+public class IndexController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "index.html";
+    }
+}
